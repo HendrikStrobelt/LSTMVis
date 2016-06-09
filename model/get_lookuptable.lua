321cc248e2b5c0f4212c0922b841db7c075cbcee
Sebastian Gehrmann <s.gehrmann@outlook.com>
2016-06-09 14:43:20 +0200

    get_lookuptable updated

--- model/get_lookuptable.lua
+++ model/get_lookuptable.lua
@@ -3,6 +3,11 @@ require 'rnn'
 require 'nngraph'
 require 'hdf5'
 
+----------------------------------------------------------------
+-- Reads the weights of a Lookuptable of a trained model.
+-- In our model, this table holds the word embeddings.
+----------------------------------------------------------------
+
 cmd = torch.CmdLine()
 cmd:option('-checkpoint_file','checkpoint/','path to model checkpoint file in t7 format')
 cmd:option('-output_file','checkpoint/lstm_states.h5','path to output LSTM states in hdf5 format')
@@ -16,18 +21,12 @@ if opt.gpuid >= 0 then
    cutorch.setDevice(opt.gpuid + 1)
 end
 
-
+--Function to recursively search through the model to find the desired layer.
 Module = nn.Module
 function Module:get_states()
-   --print("get states started")
    if self.modules then
-      --print("there are submodules")
       for i,module in ipairs(self.modules) do
-	 --print(module)
-	 --print(torch.type(module))
          if torch.type(module) == "nn.LookupTable" then
-	    --print("found the table")
-	    --print(torch.type(module.weight))
             weights =  module.weight   
          else
             module:get_states()
@@ -36,8 +35,8 @@ function Module:get_states()
    end
 end
 
-model = torch.load(opt.checkpoint_file)
- model:get_states()
+model = torch.load(opt.checkpoint_file) -- Initialize Model
+model:get_states() -- Read the Table
 local f = hdf5.open(opt.output_file, 'w')
 f:write('weights', weights:float())
 f:close()
