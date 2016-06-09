1c0eb9869f1cf3f2ac3762c2fe6461ad2d4c6e05
Sebastian Gehrmann <s.gehrmann@outlook.com>
2016-06-09 14:37:37 +0200

    Update Salience and Influence Scripts

--- model/get_influece_per_word.lua
+++ model/get_influece_per_word.lua
@@ -2,14 +2,23 @@ require 'rnn'
 require 'hdf5'
 require 'nngraph'
 
+----------------------------------------------------------------
+-- Computes the influece of each word in a context on the final 
+-- prediction and saves it. This is NOT the saliency 
+-- The data should be formatted in a shifting window. each word 
+-- should be the last word of a context once.
+----------------------------------------------------------------
+
+
 cmd = torch.CmdLine()
 
-cmd:option('-data_file','data/','data directory. Should contain data.hdf5 with input data')
+cmd:option('-data_file','data/','data')
 cmd:option('-val_data_file','data/','data directory. Should contain data.hdf5 with input data')
 cmd:option('-gpuid',-1,'which gpu to use. -1 = use CPU')
 cmd:option('-checkpoint_file','checkpoint/','path to model checkpoint file in t7 format')
 cmd:option('-output_file','reads/lstm_grads.h5','path to output LSTM gradients in hdf5 format')
 
+cmd:option('-embedding_index',2,'The index of the embedding layer in the model.')
 opt = cmd:parse(arg)
 
 
@@ -42,18 +51,9 @@ function data.__index(self, idx)
    return {input, target}
 end
 
-
-
-
-
-function get_influence(data, valid_data, model, criterion)
-   --local params, grad_params = model:getParameters()
-   
-
-   --local all_grads = torch.CudaTensor(data:size(), 35, 20, 650) 
+function get_influence(data, model, criterion)
    local all_grads = torch.CudaTensor(data.length * data.batchlength * data.seqlength, 1) 
-   --model:forget()
-   model:training()
+   model:training() -- makes sure the gradients are stored
    for i = 1, data:size() do
       if i%100 == 0 or i==2 then
          print(i, "current batch")
@@ -65,35 +65,28 @@ function get_influence(data, valid_data, model, criterion)
       local out = model:forward(input)
       -- 2. backward criterion
       local deriv = criterion:backward(out, goal)
-
-      -- 3. zero out everything except last
+      -- 3. zero out everything except gradient
       for z=1, #deriv-1 do
          deriv[z]:fill(0)
       end
-      --print('Memory!', cutorch.getMemoryUsage())
       -- 4. backward model
       model:backward(input, deriv)
-      -- print(model) 
-      -- print(model:get(1).output:size())
-      --print('Memory after backwards', cutorch.getMemoryUsage())
-      -- 5. get gradweights on lookup table for every word in input
-      --all_grads[i] = model:get(2).gradInput:clone()
-
-      local gi = model:get(2).gradInput:clone()
+      
+      --:get(X) gets the Xth module of the model. 
+      --It should point to the embedding layer.
+      local gi = model:get(opt.embedding_index).gradInput:clone()
+      --Construct the new index in the all_grads tensor 
       for csequence=1, gi:size(1) do
          for cbatch=1, gi:size(2) do
             local cindex = (i-1) * data.seqlength
             local batchbonus = (cbatch-1) * data.length
             local new_index = cindex + batchbonus + csequence
-            --print(new_index, "new_index!")
+            --compute the norm of the gradient as influence
             all_grads[new_index] = torch.norm(gi[csequence][cbatch])
 
          end
       end
-      
-      collectgarbage()
-      --break
-
+      collectgarbage() 
    end
 
    local f = hdf5.open(opt.output_file, 'w')
@@ -105,11 +98,9 @@ end
 
 
 function main() 
-    -- parse input params
+    -- Parse input params
    opt = cmd:parse(arg)
-   
    if opt.gpuid >= 0 then
-
       print('using CUDA on GPU ' .. opt.gpuid .. '...')
       require 'cutorch'
       require 'cunn'
@@ -118,18 +109,17 @@ function main()
    
    -- Create the data loader class.
    local train_data = data.new(opt, opt.data_file, opt.use_chars)
-   local valid_data = data.new(opt, opt.val_data_file, opt.use_chars)
 
-
+   -- Initialize model
    criterion = nn.SequencerCriterion(nn.ClassNLLCriterion())
    model = torch.load(opt.checkpoint_file)
-
-   
    if opt.gpuid >= 0 then
       model:cuda()
       criterion:cuda()
    end
-   get_influence(train_data, valid_data, model, criterion)
+
+   -- Compute Saliency
+   get_influence(train_data, model, criterion)
 end
 
 main()
--- model/get_saliency.lua
+++ model/get_saliency.lua
@@ -2,6 +2,12 @@ require 'rnn'
 require 'hdf5'
 require 'nngraph'
 
+----------------------------------------------------------------
+-- Computes the saliency for each word in a context and saves it
+-- The data should be formatted in a shifting window. each word 
+-- should be the last word of a context once.
+----------------------------------------------------------------
+
 cmd = torch.CmdLine()
 
 cmd:option('-data_file','data/','data directory. Should contain data.hdf5 with input data')
@@ -9,6 +15,7 @@ cmd:option('-gpuid',-1,'which gpu to use. -1 = use CPU')
 cmd:option('-checkpoint_file','checkpoint/','path to model checkpoint file in t7 format')
 cmd:option('-output_file','reads/lstm_grads.h5','path to output LSTM gradients in hdf5 format')
 
+cmd:option('-embedding_index',2,'The index of the embedding layer in the model.')
 opt = cmd:parse(arg)
 
 
@@ -42,104 +49,56 @@ function data.__index(self, idx)
 end
 
 
-
-
-
-function get_influence(data, model, criterion)
-   --local params, grad_params = model:getParameters()
-   
-
-   --local all_grads = torch.CudaTensor(data:size(), 35, 20, 650) 
+function get_salience(data, model, criterion)
    local all_grads = torch.CudaTensor(data.length * data.batchlength * data.seqlength, 1) 
-   --model:forget()
    model:training()
    for i = 1, data:size() do
       if i%100 == 0 or i==2 then
          print(i, "current batch")
       end
-
-      --DEV MODE
-      --if i == 3 then
-      --     break
-      -- end 
-      -- print(all_grads:size(1) .. " Number of gradients")
       model:forget()
       model:zeroGradParameters()
       local d = data[i]
       input, goal = d[1], d[2]
       ---1. forward
       local out = model:forward(input)
-      -- 2. backward criterion -> don't have to do that, create tensor before?
+      -- 2. backward criterion 
       local deriv = criterion:backward(out, goal)
-
       -- 3. zero out everything 
       for z=1, #deriv do
          deriv[z]:fill(0)
       end
-      -- 3.5 pretend we have the correct class
-
-      --print(deriv[#deriv][1])
-      --print(goal)
-      --print(deriv[#deriv]:size(1), "size of derivative vector")
-      --print(goal[#deriv])
+      -- 3.5 pretend we predict the correct class
       for z=1, deriv[#deriv]:size(1) do
-	 --print(goal[#deriv]:size())
-	 --print(deriv[#deriv][z])
          deriv[#deriv][z][goal[#deriv][z]] = 1
       end
-
-
-      --print('Memory!', cutorch.getMemoryUsage())
       -- 4. backward model
       model:backward(input, deriv)
-      -- print(model) 
-      -- print(model:get(1).output:size())
-      --print('Memory after backwards', cutorch.getMemoryUsage())
-      -- 5. get gradweights on lookup table for every word in input
-      
-      --print(goal[35])
-
-      local gi = model:get(2).gradInput:clone()
-      -- print(gi:size(), " Size of one batch")
+      --:get(X) gets the Xth module of the model. 
+      --It should point to the embedding layer.
+      local gi = model:get(opt.embedding_index).gradInput:clone()
+      --Construct the new index in the all_grads tensor 
       for csequence=1, gi:size(1) do
-         -- if csequence>2 then 
-         --       break
-         -- end
          for cbatch=1, gi:size(2) do
-            -- DEV MODE
-            -- if cbatch>2 then 
-            --    break
-            -- end
-            -- print(csequence, cbatch)
             local cindex = (i-1) * data.seqlength
-            -- print(cindex .. " Index-Bonus for sequence length")
             local batchbonus = (cbatch-1) * data.length *data.seqlength
-            -- print(batchbonus .. " Index-Bonus for batch number")
             local new_index = cindex + batchbonus + csequence
-            -- print(new_index .. "new index")
-            --print(new_index, "new_index!")
-
+            --compute the norm of the gradient as saliency
             all_grads[new_index] = torch.norm(gi[csequence][cbatch])
-            --print(all_grads[new_index])
 
          end
       end
       
       collectgarbage()
-      --break
-
    end
 
    local f = hdf5.open(opt.output_file, 'w')
    f:write('grads', all_grads:float())
    f:close()
-
 end
 
-
-
 function main() 
-    -- parse input params
+    -- Parse input params
    opt = cmd:parse(arg)
    
    if opt.gpuid >= 0 then
@@ -153,16 +112,15 @@ function main()
    -- Create the data loader class.
    local train_data = data.new(opt, opt.data_file, opt.use_chars)
 
-
+   -- Initialize model and criterion.
    criterion = nn.SequencerCriterion(nn.ClassNLLCriterion())
    model = torch.load(opt.checkpoint_file)
-
-   
    if opt.gpuid >= 0 then
       model:cuda()
       criterion:cuda()
    end
-   get_influence(train_data, model, criterion)
+   -- Compute Saliency
+   get_salience(train_data, model, criterion)
 end
 
 main()
